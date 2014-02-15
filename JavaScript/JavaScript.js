1188f5ac7d7acd9cfb9d9c5fbbd0c1f726e8ba7e
David Zonneveld <davidzonneveld@gmail.com>
2014-02-15 15:46:44 -0400

    First Commit

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,25 @@
+﻿var COLORS = new Array();
+COLORS[COLORS.length ? COLORS.length : 0] = "#FF0000";
+COLORS[COLORS.length ? COLORS.length : 0] = "#FFFF00";
+COLORS[COLORS.length ? COLORS.length : 0] = "#00FFFF";
+COLORS[COLORS.length ? COLORS.length : 0] = "#0000FF";
+COLORS[COLORS.length ? COLORS.length : 0] = "#FF00FF";
+var LAST = 0;
+function blinklink() {
+    LAST = (LAST + 1) % COLORS.length;
+    var _obj = null;
+    if (document.all) _obj = document.all.blink;
+    else if (document.getElementById) _obj = document.getElementById("blink");
+    if (_obj) _obj.style.color = COLORS[LAST];
+}
+
+var TIMER = null;
+var INTERVAL = 100; /* milliseconds */
+function starttimer(_id) {
+    stoptimer();
+    TIMER = setInterval("blinklink()", INTERVAL);
+}
+function stoptimer() {
+    clearTimeout(TIMER);
+    TIMER = null;
+}
